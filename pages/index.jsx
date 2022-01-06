904ff8c84560fba8e5484f7564c299d862fa3f3b
jackcrane <jack-mac@users.noreply.github.com>
2022-01-05 21:51:00 -0500

    FIx projects import

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Header from "../components/header";
 import styles from "../assets/styles/index.module.css";
 import Nav from "../components/nav";
-import Projects from "../components/Projects";
+import Projects from "../components/projects";
 import Contact from "../components/contact";
 import Skills from "../components/skills";
 
