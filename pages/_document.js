31129de3c92ad9240564488a470a4ff727ab0026
jackcrane <jack-mac@users.noreply.github.com>
2022-02-03 17:18:32 -0500

    Fix tracking

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,25 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon={JSON.stringify({
+              token: "5a90056db8424dc7adf17c0384f9078f",
+            })}
+          ></script>
+          <script
+            async
+            defer
+            src="https://sa.jackcrane.rocks/latest.js"
+          ></script>
+          <noscript>
+            <img
+              src="https://sa.jackcrane.rocks/noscript.gif"
+              alt=""
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </noscript>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
