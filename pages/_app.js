931fa466aa5071dec15cc33eaecff1a40a4cc533
Jack-Mac <jack@jackcrane.rocks>
2022-10-13 19:04:29 -0500

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import "../assets/styles/globals.css";
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+    <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-MDY1XRG17G"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MDY1XRG17G');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
